bf5125cf5b66713ba456cf8c1482a28bc9055c6a
lingaojun <44394422+lingaojun@users.noreply.github.com>
2019-12-07 15:49:03 +0800

    null

--- stderr/main.c
+++ stderr/main.c
@@ -3,12 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-    fprintf(stderr, "EACCES: %s\n", strerror(EACCES));//其作用是格式化输出到一个流文件(stderr)中
-    printf("%s", strerror(EACCES)); //这行不输出，说明只能将该字符串与对应终端的屏幕stderr组合使用
+    fprintf(stderr, "EACCES: %s\n", strerror(EACCES));
+    printf(strerror(EACCES));
     errno = ENOENT;
-    perror(argv[0]);//此时的错误码errno = ENOENT,表示文件不存在，那么此时输出应该是 ./a.out: No such file or directory
-	errno = EACCES;
-    perror(argv[0]);
+    perror(argv[0]); //将错误码的意义作为argv[0]的后缀，例如当错误码errno = ENOENT时，表示的意思是找不到该文件，那么就会显示，./a.out: No such file or directory.
     exit(0);
     return 0;
 }
