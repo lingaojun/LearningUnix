8a2d6f41833e4c19dcf78cee191a51d374821bbd
lingaojun <44394422+lingaojun@users.noreply.github.com>
2019-12-09 09:49:34 +0800

    Update main.c

--- file/ungetc/main.c
+++ file/ungetc/main.c
@@ -4,9 +4,9 @@
 int main(void)
 {
     FILE *fp;
-    char *str = "hello world";
+    char *str = "hello world\n";
     char *buffer;
-    int i = 0;
+    int c = 0;
     fp  = fopen("test.txt", "wb+");
     printf("strlen is %d\n", strlen(str));
     fwrite(str, sizeof(char), strlen(str), fp);
@@ -14,21 +14,15 @@ int main(void)
     rewind(fp);
     while(!feof(fp))
     {
-        int c = getc(fp); //从fp中读出字符
+        c = fgetc(fp); //从fp中读出字符
         if (c == 'l') {
-        ungetc('p', fp); //若满足条件，则将p重新压进fp中
-        continue;
+	        ungetc('p', fp); //若满足条件，则将p重新压进fp中
+	        continue;
         }
+        if (c != -1)
         fputc(c, stdout);
-        buffer[i++] = c;
-        /*else
-        ungetc(c, fp);  //若不满足则重新压进fp中，保证fp的完整性。
-        //fgets(buffer, sizeof(buffer), fp); //把流内容依次读出来
-        fgets(buffer, sizeof(buffer), fp);
-        fputs(buffer, stdout);
-        */
     }
-    printf("%s\n", buffer);
+
     fclose(fp);
     return 0;
 }
