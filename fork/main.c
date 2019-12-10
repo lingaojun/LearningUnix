bdd3a36512ac0348d2c54ca14fa833e3650e32bf
lingaojun <44394422+lingaojun@users.noreply.github.com>
2019-12-10 16:17:32 +0800

    Update main.c

--- fork/main.c
+++ fork/main.c
@@ -5,6 +5,7 @@ int main(void)
 {
 	pid_t pid;
 	int count = 0;
+    printf("start\n");
     pid = fork();
     if (pid < 0)
     {
@@ -14,15 +15,18 @@ int main(void)
     {
 		printf("this is a son\n");
         printf("the pid is %d\n", getpid());
-        return 0;
+        printf("the uid is %d\n", getuid());
+        printf("the gid is %d\n", getgid());
     }
     else
     {
+        sleep(1);
 		printf("this is a father\n");
         printf("the pid is %d\n", getpid());
-        return 0;
+        printf("the uid is %d\n", getuid());
+        printf("the gid is %d\n", getgid());
     }
 
-
+    printf("end\n");
 
 }
