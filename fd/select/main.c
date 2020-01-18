5a0bf4efafd55f3c539703b0309636dffa814f0b
lingaojun <44394422+lingaojun@users.noreply.github.com>
2020-01-18 12:01:44 +0800

    NULL
    NULL

---
+++ fd/select/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <sys/select.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+    fd_set readfds, writefds;//读, 写文件描述符
+    int ready, nfds, fd;
+    struct timeval timeout;//超时
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+
+    FD_ZERO(&readfds);
+    FD_ZERO(&writefds);
+
+    for (int i = 0; i < 4; i++) { // 4改为3即只测试前三个系统fd即可select正确返回，第4个fd未进行具体赋值，所以select出错。
+		fd = i;
+		printf("the fd is %d\n", fd);
+		nfds = i+1;
+		//FD_SET(fd, &readfds);
+		FD_SET(fd, &writefds);
+    }
+
+
+    ready = select(nfds, &readfds, &writefds, NULL, &timeout);
+    printf("errno is %d\n", errno);
+    printf("ready = %d\n", ready);
+    for (fd = 0; fd < nfds; fd++)
+        printf("%d: %s%s\n", fd, FD_ISSET(fd, &readfds) ? "r" : "",
+                FD_ISSET(fd, &writefds) ? "w" : "");
+
+}
+
